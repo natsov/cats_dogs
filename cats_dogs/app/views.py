006dda1ebd1eb672d67924c31ed00f68b1c4f123
natalia soupel <sov.holms@gmail.com>
2024-11-06 23:33:43 +0300

    add requirements.txt

--- cats_dogs/app/views.py
+++ cats_dogs/app/views.py
@@ -1,22 +1,33 @@
-import os
 import torch
-from django.utils.decorators import method_decorator
-from torchvision import models, transforms
 from django.shortcuts import render
-from django.http import JsonResponse
+from torchvision import models, transforms
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from django.views import View
 from PIL import Image
 from torchvision.models import ResNet18_Weights
+import os
 
 
 class ImageClassifier:
-    def __init__(self, model_path):
+    _instance = None
+
+    def __new__(cls, model_path, device='cpu'):
+        if cls._instance is None:
+            cls._instance = super(ImageClassifier, cls).__new__(cls)
+            cls._instance.initialize(model_path, device)
+        return cls._instance
+
+    def initialize(self, model_path, device):
+        self.device = torch.device(device)
         self.model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
         num_classes = 2
         self.model.fc = torch.nn.Linear(self.model.fc.in_features, num_classes)
+
         try:
-            self.model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+            self.model.load_state_dict(torch.load(model_path, map_location=self.device))
+            self.model.to(self.device)
             self.model.eval()
         except RuntimeError as e:
             print(f"Model loading error: {e}")
@@ -30,7 +41,7 @@ class ImageClassifier:
     def classify_image(self, image_file):
         try:
             image = Image.open(image_file).convert('RGB')
-            image = self.transform(image).unsqueeze(0)
+            image = self.transform(image).unsqueeze(0).to(self.device)
             with torch.no_grad():
                 output = self.model(image)
                 _, predicted = torch.max(output, 1)
@@ -39,7 +50,6 @@ class ImageClassifier:
         except Exception as e:
             raise ValueError("Image processing error: " + str(e))
 
-classifier = ImageClassifier(os.getenv('MODEL_PATH'))
 
 class ClassifyView(View):
     @method_decorator(csrf_exempt)
@@ -48,11 +58,14 @@ class ClassifyView(View):
         if not image_file:
             return JsonResponse({'error': 'No image provided'}, status=400)
 
+        classifier = ImageClassifier(os.getenv('MODEL_PATH', 'models/model.pth'))
+
         try:
             label = classifier.classify_image(image_file)
             return JsonResponse({'label': label})
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+
 
 def main(request):
     return render(request, 'site/main.html', {})
